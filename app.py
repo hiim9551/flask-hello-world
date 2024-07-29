f7818f44dccf47c75816f6136819285427627f31
hiim9551 <hiim9551@colorado.edu>
2024-07-29 04:34:43 +0000

    Fixed bug

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello World from Hitomi Imai in 3308'
 
 @app.route('/db_test')
-def testing:
+def testing():
     conn = psycopg2.connect("postgresql://lab10_render_example_user:G5dC179D1ioxdA6j1h6OFxFjr3FQMyXO@dpg-cqj0td8gph6c738vfho0-a/lab10_render_example")
     conn.close()
     return "Database Connection Successful"
